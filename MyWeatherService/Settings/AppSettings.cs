0def71847ed454be035a7b21b8920383d5eb54b8
zotov_dv <zotov_dv@proitr.ru>
2020-04-24 00:41:19 +0300

    DefaultLocation property added to appSettings

--- MyWeatherService/Settings/AppSettings.cs
+++ MyWeatherService/Settings/AppSettings.cs
@@ -4,12 +4,13 @@ namespace MyWeatherService.Settings
 {
     public class AppSettings
     {
-        public AppSettings(Dictionary<string, string> cStrings, Dictionary<string, double> intervals, OpenCageData openCageData, OpenWeatherData openWeatherData)
+        public AppSettings(Dictionary<string, string> cStrings, Dictionary<string, double> intervals, OpenCageData openCageData, OpenWeatherData openWeatherData, DefaultLocation defaultLocation)
         {
             ConnectionStrings = cStrings;
             Intervals = intervals;
             OpenCageData = openCageData;
             OpenWeatherData = openWeatherData;
+            DefaultLocation = defaultLocation;
         }
 
         public OpenCageData OpenCageData { get; protected set; }
