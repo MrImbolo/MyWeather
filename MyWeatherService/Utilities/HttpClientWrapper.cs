23cf8fabc228fda48d23ff6bcd1ded79c54ee00e
zotov_dv <zotov_dv@proitr.ru>
2020-04-24 00:42:30 +0300

    Updated HttpClientWrapper to have ability to recieve byte[] and stream HttpRequestMessage

--- MyWeatherService/Utilities/HttpClientWrapper.cs
+++ MyWeatherService/Utilities/HttpClientWrapper.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.IO;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -9,29 +10,63 @@ namespace MyWeatherService.Utilities
     {
         static readonly HttpClient client = new HttpClient();
 
-        public static async Task<string> Request(HttpRequestMessage message, CancellationToken stoppingToken, ILogger logger)
+        public static async Task<string> RequestString(HttpRequestMessage message, CancellationToken stoppingToken, ILogger logger)
         {
             // Call asynchronous network methods in a try/catch block to handle exceptions.
 
             try
             {
-                while(!stoppingToken.IsCancellationRequested)
-                {
-                    HttpResponseMessage response = await client.SendAsync(message);
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    // Above three lines can be replaced with new helper method below
-                    // string responseBody = await client.GetStringAsync(uri);
+                HttpResponseMessage response = await client.SendAsync(message, stoppingToken);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
 
-                    return responseBody;
-                }
-                logger.LogInformation("Request cancelled by user");
-                return "{'status': { 'Name': 'Error', 'StatusCode': '499', 'Message': 'Cancelled by user' }}";
+                return responseBody;
             }
             catch (HttpRequestException e)
             {
                 logger.LogError(e, "Request error!");
                 return string.Empty;
+            }
+        }
+        public static async Task RequestStream(HttpRequestMessage message, CancellationToken stoppingToken, ILogger logger, Stream stream)
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+
+            try
+            {
+                HttpResponseMessage response = await client.SendAsync(message, stoppingToken);
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStreamAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                await responseBody.CopyToAsync(stream);
+            }
+            catch (HttpRequestException e)
+            {
+                logger.LogError(e, "Request error!");
+            }
+        }
+        public static async Task<byte[]> RequestByteArray(HttpRequestMessage message, CancellationToken stoppingToken, ILogger logger)
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+
+            try
+            {
+                HttpResponseMessage response = await client.SendAsync(message, stoppingToken);
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsByteArrayAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                logger.LogError(e, "Request error!");
+                return new byte[] { };
             }
         }
     }
