b1dbd045c42286bab9f4b4ea80d001ae2168bd30
zotov_dv <zotov_dv@proitr.ru>
2020-04-23 16:16:13 +0300

    MyWeather app initial commit - added default worker service and dal class lib

---
+++ MyWeatherDAL/MyContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace MyWeatherDAL
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions options) : base(options) { }
+
+    }
+}
