8fb57e7b669a6304ded19988578cb6405d275193
zotov_dv <zotov_dv@proitr.ru>
2020-04-23 19:22:16 +0300

    DTOs added to context

--- MyWeatherDAL/MyContext.cs
+++ MyWeatherDAL/MyContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DTOs.Weathers;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,13 @@ namespace MyWeatherDAL
     {
         public MyContext(DbContextOptions options) : base(options) { }
 
+        DbSet<Current> Currents { get; set; }
+        DbSet<Daily> Dailies { get; set; }
+        DbSet<FeelsLike> FeelsLikes{ get; set; }
+        DbSet<Hourly> Hourlies{ get; set; }
+        DbSet<Rain> Rains{ get; set; }
+        DbSet<Temp> Temps{ get; set; }
+        DbSet<Weather> Weathers { get; set; }
+        DbSet<WeatherSummary> WeatherSummaries { get; set; }
     }
 }
